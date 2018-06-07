58b7eee4ac3a110e0b5b4701ebc26c91d24ec50e
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-06-07 19:51:32 +0200

    Explicit the distinction between world simulation and display

--- main.cpp
+++ main.cpp
@@ -1,8 +1,52 @@
 #include "SDL.h"
 
+SDL_Renderer* renderer;
+
+auto const SPEED = 16;
+
 struct World
 {
+  int pos = 0;
+  int vel = 0;
+
+  void update()
+  {
+    if(pos > 1900)
+      vel = -SPEED;
+    if(pos < 10)
+      vel = SPEED;
+    pos += vel;
+  }
 };
+
+World world;
+
+void drawScreen(int deltaTime)
+{
+  SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+  SDL_RenderFillRect(renderer, nullptr);
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_Rect rect {};
+  rect.x = 10 + world.pos;
+  rect.y = 10;
+  rect.w = 100;
+  rect.h = 1000;
+  SDL_RenderFillRect(renderer, &rect);
+
+
+  {
+    SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+    SDL_Rect rect {};
+    rect.x = 10;
+    rect.y = 10;
+    rect.w = 10;
+    rect.h = 10 + deltaTime * 10;
+    SDL_RenderFillRect(renderer, &rect);
+  }
+
+  SDL_RenderPresent(renderer);
+}
 
 int main()
 {
@@ -12,11 +56,7 @@ int main()
   flags |= SDL_RENDERER_ACCELERATED;
   flags |= SDL_RENDERER_PRESENTVSYNC;
   auto wnd = SDL_CreateWindow("Smooth", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_FULLSCREEN_DESKTOP);
-  auto renderer = SDL_CreateRenderer(wnd, -1, flags);
-
-  auto const speed = 16;
-  int pos = 0;
-  int vel = speed;
+  renderer = SDL_CreateRenderer(wnd, -1, flags);
 
   auto time = SDL_GetTicks();
 
@@ -25,37 +65,9 @@ int main()
     auto const deltaTime = SDL_GetTicks() - time;
     time += deltaTime;
 
-    if(pos > 1900)
-      vel = -speed;
-    if(pos < 10)
-      vel = speed;
-    pos += vel;
+    world.update();
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-    SDL_RenderFillRect(renderer, nullptr);
-
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect rect {};
-    rect.x = 10 + pos;
-    rect.y = 10;
-    rect.w = 100;
-    rect.h = 1000;
-    SDL_RenderFillRect(renderer, &rect);
-
-
-    {
-      SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-      SDL_Rect rect {};
-      rect.x = 10;
-      rect.y = 10;
-      rect.w = 10;
-      rect.h = 10 + deltaTime * 10;
-      SDL_RenderFillRect(renderer, &rect);
-    }
-
-
-    SDL_RenderPresent(renderer);
-  //  SDL_Delay(16);
+    drawScreen(deltaTime);
   }
 
   SDL_DestroyWindow(wnd);
